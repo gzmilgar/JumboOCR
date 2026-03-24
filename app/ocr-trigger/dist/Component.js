b9fa5a6859ebc4cff9a2e14428d9f469f77db4a5
Gizem ilgar <gzmilgar@gmail.com>
2026-03-24 13:50:02 +0000

    Added App

--- app/ocr-trigger/dist/Component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("com.jumbo.ocr.ocrtrigger.Component",{metadata:{manifest:"json"}})});
-//# sourceMappingURL=Component.js.map
